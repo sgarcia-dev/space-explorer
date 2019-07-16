b25385a695461e93f6302b91c40ff34e5fb9fab2
Sergei Garcia Ortega <sergei.garcia@digitalonus.com>
2019-07-16 16:31:08 -0500

    feat: add local state

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,33 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
-
-import { ApolloProvider } from 'react-apollo';
+import { Query, ApolloProvider } from 'react-apollo';
+import gql from 'graphql-tag';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import Pages from './pages';
+import Login from './pages/login';
+import injectStyles from './styles';
+import { resolvers, typeDefs } from './resolvers';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
   uri: 'http://localhost:4000/'
 })
 
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
 const client = new ApolloClient({
   cache,
   link,
+  typeDefs,
+  resolvers,
   headers: {
     authorization: localStorage.getItem('token'),
   },
@@ -27,8 +40,11 @@ cache.writeData({
   },
 });
 
+injectStyles();
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
+    <Query query={IS_LOGGED_IN}>
+      {({ data }) => (data.isLoggedIn ? <Pages /> : <Login />)}
+    </Query>
   </ApolloProvider>, document.getElementById('root')
 );
