7e8e36d82339393c14382d4a2850c63ad5f3b020
Sergei Garcia Ortega <sergei.garcia@digitalonus.com>
2019-07-15 20:27:40 -0500

    feat: add missing mutation resolvers

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,4 @@
+const isEmail = require('isemail');
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
@@ -13,7 +14,19 @@ const server = new ApolloServer({
   dataSources: () => ({
     launchAPI: new LaunchAPI(),
     userAPI: new UserAPI({ store })
-  })
+  }),
+  context: async ({ req }) => {
+    // simple auth check on every request
+    const auth = (req.headers && req.headers.authorization) || '';
+    const email = Buffer.from(auth, 'base64').toString('ascii');
+    // if the email isn't formatted validly, return null for user
+    if (!isEmail.validate(email)) return { user: null };
+    // find a user by their email
+    const users = await store.users.findOrCreate({ where: { email } });
+    const user = users && users[0] ? users[0] : null;
+
+    return { user: { ...user.dataValues } };
+  },
 });
 
 server.listen().then(({ url }) => {
